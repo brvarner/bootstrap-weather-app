9ec20537063f6bbb1922ae90a865a67e80d08d81
brvarner <bvarner4592@gmail.com>
2022-12-09 12:41:55 -0600

    removed empty object in functional component that was triggering warning

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "../../components";
 
-export default function AppContainer({}) {
+export default function AppContainer() {
   return <Header />;
 }
