16d91eea8f483938dde91094518de73fa09a05ef
brvarner <bvarner4592@gmail.com>
2022-12-09 12:46:02 -0600

    fixed mixed content error

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -4,7 +4,7 @@ export const request = async (city) => {
   // This is the Geozoning API endpoint that we can use to grab the information
   // about our city's latitude and longitude before submitting our request to
   // the OpenWeather API.
-  const baseURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city.trim()}&limit=5&appid=${key}`;
+  const baseURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city.trim()}&limit=5&appid=${key}`;
   try {
     const response = await fetch(baseURL);
     const json = await response.json();
